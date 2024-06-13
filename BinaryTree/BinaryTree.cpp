cc983c972359a68d898f75d19547879e545c6e20
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-06-13 15:22:59 +0700

    assign value to the data field of the new node

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -30,6 +30,7 @@ public:
 	void insert(string element) // insert a node in the binary search tree
 	{
 		Node* newNode = new Node(element, nullptr, nullptr);
+		newNode->info = element; // assign value to the data field of the new node
 	}
 };
 
